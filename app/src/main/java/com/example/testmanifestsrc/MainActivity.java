1752b476e014ebec74bccb0882483ea7e882aebe
Kevin Barry <kevin@teslacoilsw.com>
2022-02-08 01:57:21 -0600

    initial commit

---
+++ app/src/main/java/com/example/testmanifestsrc/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.testmanifestsrc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.hello_world)).setText(TestClass.getText());
+    }
+}
---
+++ app/src/main/java/com/example/testmanifestsrc/TestClass.java
@@ -0,0 +1,8 @@
+package com.example.testmanifestsrc;
+
+public class TestClass {
+
+    public static String getText() {
+        return "Test Class Text";
+    }
+}
